ba00bd5e2733d88047101beb3c2506d4c2d10cea
Kiana Mills <kiana.mills20@gmail.com>
2026-01-10 14:26:53 -0800

    created state functs

--- index.js
+++ index.js
@@ -11,3 +11,45 @@ let oddNumbers = [];
 
 let evenNumbers = [];
 
+function addNumber(num) {
+   numberBank.push(num);
+    render()
+}
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+function isOdd(num) {
+    return num % 2 !== 0;
+}
+// added isEven and isOdd for later to help sort in my state functions
+
+function sortOne() {
+    if(numberBank.length === 0) {
+        return;
+    }
+    const firstNum = numberBank.shift();
+    if(isEven(firstNum)) {
+        evenNumbers.push(firstNum);
+    }else {
+        oddNumbers.push(firstNum);
+    }
+    render()
+} 
+//takes the first number of numberBank array and is now pushing it to 
+//the correct even or odds array
+
+function sortAll() {
+    for(let i = 0; i < numberBank.length; i++) {
+        const num = numberBank[i];
+        if(isEven(num)) {
+            evenNumbers.push(num);
+        }else {
+            oddNumbers.push(num);
+        }
+    }
+    numberBank = [];
+    render()
+}
+//created a loop so that it loops through all of the numberBank
+//and ends up in even or odds
