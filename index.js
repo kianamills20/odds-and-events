9f4fb6239eeb3992eacedf4c2424aa6919052df8
Kiana Mills <kiana.mills20@gmail.com>
2026-01-10 11:37:38 -0800

    wrote to-dos & built arr

---
+++ index.js
@@ -0,0 +1,13 @@
+/* 1. need three state arrays Bank, Odds, and Evens
+2. state change functions like add number, sort 1, and sort all
+3. write some component functions the create DOM elements
+4. add some event listeners that call state-changes
+5. render!!
+ */ 
+
+let numberBank = [];
+
+let oddNumbers = [];
+
+let evenNumbers = [];
+
